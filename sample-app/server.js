e2a9aa5cb8b2675b271a2debb02f1aa8fa05d6c9
jay <jay@decompany.io>
2020-03-09 16:03:42 +0900

    Update callback 리턴시 return_url, expired_at 추가

--- sample-app/server.js
+++ sample-app/server.js
@@ -1,17 +1,24 @@
-'use strict';
-const PORT = 3000;
+'use strict'
+
+const PORT = 3000
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.redirect('/serverless-authentication-gh-pages');
+    res.redirect('/serverless-authentication-gh-pages')
    //res.send('Hello World!')
 })
 
+app.get('/callback', (req, res) => {
+    res.sendFile('/public/index.html', { root: __dirname })
+})
+
+
+
 app.get('/serverless-authentication-gh-pages', (req, res) => {
     console.log(__dirname)
-    res.sendFile('/public/index.html', { root: __dirname });
+    res.sendFile('/public/index.html', { root: __dirname })
 })
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
